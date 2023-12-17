d41691244426a2d9480ce32e7c16726d7bc2e85b
Paweł <pawelmieszko05@gmail.com>
2023-12-17 01:07:27 +0100

    final final

--- kol_test/valgrind_test/btester.cpp
+++ kol_test/valgrind_test/btester.cpp
@@ -17,7 +17,7 @@ using namespace std;
 // Odkomentuj #undef DEBUG aby pozbyć się wypisywania kroków.
 
 #define DEBUG
-//#undef DEBUG
+#undef DEBUG
 
 #ifdef DEBUG
 #define debug(X) cout << X
@@ -42,8 +42,8 @@ int main()
     
     
     // Zakresy można modyfikować aby generować duże/małe testy.
-    int m = randint(1, 10);
-    int q = randint(1, 10);
+    int m = randint(1, 1000);
+    int q = randint(1, 1000);
     
     debug("m = " << m << "\n" << flush);
     
